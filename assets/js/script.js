3900c2933d0543539af61b262998806959bab8cb
7acuerda <rodrigo@7acuerda.cl>
2022-12-20 20:01:39 -0300

    segundo

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const imgBorder = document.querySelector('#imgBorder')
+const imgCambiar = document.querySelector('.imgCambiar')
+
+imgCambiar.addEventListener("click", () => {
+    imgBorder.classList.toggle ("border")
+} )
